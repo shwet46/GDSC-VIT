395b9a9a3ac6c8694c91b11854fd60723e392145
shwet46 <143203479+shwet46@users.noreply.github.com>
2023-09-07 18:16:50 +0530

    Add files via upload

---
+++ pgm2.java
@@ -0,0 +1,19 @@
+public class pgm2 {
+    public static void main(String[] args) {
+
+
+        int sum;
+        int prev = 0;
+        int curr = 1;
+
+        while(curr<=10){
+            sum = prev + curr;
+            System.out.println(sum);
+            prev ++;
+            curr ++;
+        }
+
+
+
+    }
+}
