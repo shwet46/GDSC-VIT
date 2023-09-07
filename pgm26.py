dfbbdef1687879fb85721119e859dc7050fc6eb6
shwet46 <143203479+shwet46@users.noreply.github.com>
2023-09-07 19:48:07 +0530

    Update pgm26.py

--- pgm26.py
+++ pgm26.py
@@ -1,13 +1,1 @@
 
-with open('filekey.key', 'rb') as filekey:
-    key = filekey.read()
- 
-fernet = Fernet(key)
- 
-with open('nba.csv', 'rb') as file:
-    original = file.read()
-     
-encrypted = fernet.encrypt(original)
- 
-with open('nba.csv', 'wb') as encrypted_file:
-    encrypted_file.write(encrypted)
