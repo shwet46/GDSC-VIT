53fea9d0537474f3474ceb6f2dab02c218d11403
shwet46 <143203479+shwet46@users.noreply.github.com>
2023-09-07 18:28:19 +0530

    Add files via upload

---
+++ pgm20.java
@@ -0,0 +1,14 @@
+public class pgm20 {
+    public static void main(String[] args) {
+
+        String s = "vidyalankar";
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(s);
+        sb.reverse();
+
+        System.out.println(sb);
+
+    }
+}
